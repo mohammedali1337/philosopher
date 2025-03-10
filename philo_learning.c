7de57a244093c5d3d6e41f061a79710adbb1459f
mohammedali1337 <mohammedgarouj7@gmail.com>
2025-03-10 20:26:55 +0000

    add some exemple for mutex working

---
+++ philo_learning.c
@@ -0,0 +1,31 @@
+#include "philo.h"
+
+pthread_mutex_t lock;
+int counter = 0;
+
+void* increment(void *arg)
+{
+    for(int i = 0; i < 100000; i++)
+    {
+        pthread_mutex_lock(&lock);
+        counter++;
+        pthread_mutex_unlock(&lock);
+    }
+    return NULL;
+}
+
+int main()
+{
+    pthread_t t1, t2;
+    pthread_mutex_init(&lock, NULL);
+    pthread_create(&t1, NULL, increment, NULL);
+    pthread_create(&t2, NULL, increment, NULL);
+    
+    pthread_join(t1 , NULL);
+    pthread_join(t2 , NULL);
+
+    pthread_mutex_destroy(&lock);
+
+    printf("counter = %d \n", counter);
+    return 0;
+}
