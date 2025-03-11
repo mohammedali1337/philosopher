848840783431337ebc992db6cf4529407351a8e5
mohammedali1337 <mohammedgarouj7@gmail.com>
2025-03-11 20:32:19 +0000

    add num of philo

--- mandatory/main.c
+++ mandatory/main.c
@@ -6,7 +6,7 @@
 /*   By: mgarouj <mgarouj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 23:46:34 by mgarouj           #+#    #+#             */
-/*   Updated: 2025/03/11 01:07:47 by mgarouj          ###   ########.fr       */
+/*   Updated: 2025/03/11 20:25:49 by mgarouj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	main(int c, char **v)
 {
-	t_philo philo;
+	t_table table;
 	if (c != 5 && c != 6)
-		return 1;
-	if (!valide_input(v, &philo))
+		return (printf("Usage: ./philo number_of_philos time_to_die time_to_eat time_to_sleep [must_eat]\n"), 1);
+	if (!valide_input(v, &table))
 		return 1;
 }
--- mandatory/parsing.c
+++ mandatory/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mgarouj <mgarouj@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 00:39:38 by mgarouj           #+#    #+#             */
-/*   Updated: 2025/03/11 19:47:10 by mgarouj          ###   ########.fr       */
+/*   Updated: 2025/03/11 20:31:16 by mgarouj          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,62 @@ int	ft_isdigit(char **v)
     return (1);
 }
 
-int valide_input(char **v, t_philo *philo)
+static int	sign_spc(char *str, int *skip)
+{
+	int	i;
+	int	sign;
+
+	i = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	*skip = i;
+	return (sign);
+}
+
+int	ft_atoi(const char *str)
+{
+	int			i;
+	int			sign;
+	long long	result;
+	long long	o;
+
+	i = 0;
+	result = 0;
+	sign = sign_spc((char *)str, &i);
+	while (((char *)str)[i] >= '0' && ((char *)str)[i] <= '9')
+	{
+		o = result;
+		result = result * 10 + (((char *)str)[i++] - 48);
+		if ((result / 10) != o)
+		{
+			if (sign > 0)
+				return (-1);
+			return (0);
+		}
+	}
+	return (result * sign);
+}
+
+int valide_number_of_philo(char **v, t_table *table)
+{
+    table->num_philos = ft_atoi(v[1]);
+    if (table->num_philos <= 0)
+        return (0);
+    return (1);
+}
+
+int valide_input(char **v, t_table *table)
 {
     if (!ft_isdigit(v))
         return (0);
-    
+    if (!valide_number_of_philo(v, table))
+        return (1);
     return 1;
 }
